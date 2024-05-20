d35ae0116c4fb75b46a8d358ab112d5c875b602a
arunmhere62 <120382469+arunmhere62@users.noreply.github.com>
2024-05-20 10:38:39 +0530

    url

--- src/constants/api-urls.ts
+++ src/constants/api-urls.ts
@@ -4,7 +4,7 @@
 const JAVA_URL = "https://ims-backend-9ghn.onrender.com"; // ! java backend
 const LOCAL_URL = "http://localhost:4000";
 const NODE_URL = "https://node-js-invoice.onrender.com";  // ! nodejs backend
-export const BASE_LOCAL_URL = NODE_URL;
+export const BASE_LOCAL_URL = JAVA_URL;
 
 export enum ApiEndpoint {
     LOGIN = "login",
