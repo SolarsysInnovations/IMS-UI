9b6b58181c82e3f20d372782b91bc0f19f60acca
arunmhere62 <120382469+arunmhere62@users.noreply.github.com>
2024-05-20 11:18:54 +0530

    error : in react-toast-editor

--- src/pages/Invoice/Send-email.tsx
+++ src/pages/Invoice/Send-email.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Form, Formik } from "formik";
-import { Editor } from "@toast-ui/react-editor";
-import "@toast-ui/editor/dist/toastui-editor.css";
-import { Box, Grid, Button ,Typography} from "@mui/material";
+// import { Editor } from "@toast-ui/react-editor";
+// import "@toast-ui/editor/dist/toastui-editor.css";
+import { Box, Grid, Button, Typography } from "@mui/material";
 import TextFieldUi from "../../components/ui/TextField";
 import ButtonSmallUi from "../../components/ui/ButtonSmall";
 import { useSendEmailNotifictionMutation } from "../../redux-store/invoice/invcoiceApi";
@@ -39,7 +39,7 @@ const SendEmail = () => {
   };
 
   const toolbarItems = [
-    ['heading', 'bold', 'italic', 'strike', 'hr', 'quote', 'ul', 'ol','table','indent','outdent','image', 'link']
+    ['heading', 'bold', 'italic', 'strike', 'hr', 'quote', 'ul', 'ol', 'table', 'indent', 'outdent', 'image', 'link']
   ];
 
   const VisuallyHiddenInput = styled("input")({
@@ -82,38 +82,38 @@ const SendEmail = () => {
     <>
       <Formik initialValues={emailValues} validationSchema={sendEmailValidationSchema} onSubmit={handleSubmit}>
         {({ values, errors, touched, handleChange, handleSubmit, setFieldValue }) => (
-            <div>
-              <Form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column" }}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <Box>
-                      <Typography variant="h5" color="initial">
-                        {pathname}
-                      </Typography>
-                    </Box>
-                  </Grid>
-                  <Grid item xs={10}>
-                      <Box>
-                        <TextFieldUi
-                            required={true}
-                            fullWidth={false}
-                            label="From Email"
-                            name="fromemail"
-                            type="email"
-                            value={(() => {
-                              return values.fromemail;
-                            })()}
-                            onChange={handleChange}
-                            error={touched.fromemail && Boolean(errors.fromemail)}
-                            helperText={touched.fromemail && errors.fromemail}
-                          />
-                      </Box>
-                  </Grid>
-                  {/* {errors.fromemail && touched.fromemail && (
+          <div>
+            <Form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column" }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Box>
+                    <Typography variant="h5" color="initial">
+                      {pathname}
+                    </Typography>
+                  </Box>
+                </Grid>
+                <Grid item xs={10}>
+                  <Box>
+                    <TextFieldUi
+                      required={true}
+                      fullWidth={false}
+                      label="From Email"
+                      name="fromemail"
+                      type="email"
+                      value={(() => {
+                        return values.fromemail;
+                      })()}
+                      onChange={handleChange}
+                      error={touched.fromemail && Boolean(errors.fromemail)}
+                      helperText={touched.fromemail && errors.fromemail}
+                    />
+                  </Box>
+                </Grid>
+                {/* {errors.fromemail && touched.fromemail && (
                     <div style={{ color: "red" }}>{errors.fromemail}</div>
                   )} */}
-                  <Grid item xs={10}>
-                    <Box>
+                <Grid item xs={10}>
+                  <Box>
                     <TextFieldUi
                       required={true}
                       fullWidth={false}
@@ -125,71 +125,71 @@ const SendEmail = () => {
                       error={touched.recipientEmail && Boolean(errors.recipientEmail)}
                       helperText={touched.recipientEmail && errors.recipientEmail}
                     />
-                    </Box>
-                  </Grid>
-                  {/* {errors.recipientEmail && touched.recipientEmail && (
+                  </Box>
+                </Grid>
+                {/* {errors.recipientEmail && touched.recipientEmail && (
                     <div style={{ color: "red" }}>{errors.recipientEmail}</div>
                   )} */}
-                  <Grid item xs={12}>
-                    <Box>
-                      <Editor
-                        initialValue=""
-                        previewStyle="vertical"
-                        height="280px"
-                        initialEditType="wysiwyg"
-                        hideModeSwitch={true}
-                        toolbarItems={toolbarItems}
-                        ref={(editor: any) => setEditor(editor)}
+                <Grid item xs={12}>
+                  <Box>
+                    {/* <Editor
+                      initialValue=""
+                      previewStyle="vertical"
+                      height="280px"
+                      initialEditType="wysiwyg"
+                      hideModeSwitch={true}
+                      toolbarItems={toolbarItems}
+                      ref={(editor: any) => setEditor(editor)}
+                    /> */}
+                  </Box>
+                </Grid>
+              </Grid>
+              <Grid container spacing={2}>
+                <Grid item xs={10}>
+                  <Box sx={{ mt: 3, display: "flex" }}>
+                    <Button
+                      component="label"
+                      role={undefined}
+                      variant="contained"
+                      tabIndex={-1}
+                      startIcon={<CloudUploadIcon />}
+                    >
+                      Upload file
+                      <VisuallyHiddenInput
+                        id="fileInput"
+                        type="file"
+                        onChange={(event) => {
+                          handleFileUpload(event);
+                          setFieldValue("file", event.currentTarget.files?.[0] || null);
+                        }}
                       />
-                    </Box>
-                  </Grid>
+                    </Button>
+                  </Box>
                 </Grid>
-                <Grid container spacing={2}>
-                  <Grid item xs={10}>
-                    <Box sx={{ mt: 3, display: "flex" }}>
-                      <Button
-                        component="label"
-                        role={undefined}
-                        variant="contained"
-                        tabIndex={-1}
-                        startIcon={<CloudUploadIcon />}
-                      >
-                        Upload file
-                        <VisuallyHiddenInput
-                          id="fileInput"
-                          type="file"
-                          onChange={(event) => {
-                            handleFileUpload(event);
-                            setFieldValue("file", event.currentTarget.files?.[0] || null);
-                          }}
-                        />
-                      </Button> 
-                    </Box>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Box sx={{ mt: 3, display: "flex" }}>
-                      <ButtonSmallUi color="primary" label="Send" size="small" variant="contained" type="submit"/>
-                    </Box>
-                  </Grid>
+                <Grid item xs={2}>
+                  <Box sx={{ mt: 3, display: "flex" }}>
+                    <ButtonSmallUi color="primary" label="Send" size="small" variant="contained" type="submit" />
+                  </Box>
                 </Grid>
-                <Grid container spacing={1}>
-                  {uploadedFiles && uploadedFiles.map((fileName, index) => (
-                    <>
-                      <Grid item xs={5}>
-                        <Box sx={{ mt: 1, display: "flex" }}>
-                          <Typography>{fileName}</Typography>
-                        </Box>
-                      </Grid>
-                      <Grid item xs={7}>
-                        <Box sx={{ mt: 1, display: "flex" }}>
-                          <ButtonSmallUi color="secondary" onClick={() => handleRemoveFile(index)} label="Remove"/>
-                        </Box>
-                      </Grid>
-                    </>
-                  ))}
-                </Grid>
-              </Form>
-            </div>
+              </Grid>
+              <Grid container spacing={1}>
+                {uploadedFiles && uploadedFiles.map((fileName, index) => (
+                  <>
+                    <Grid item xs={5}>
+                      <Box sx={{ mt: 1, display: "flex" }}>
+                        <Typography>{fileName}</Typography>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={7}>
+                      <Box sx={{ mt: 1, display: "flex" }}>
+                        <ButtonSmallUi color="secondary" onClick={() => handleRemoveFile(index)} label="Remove" />
+                      </Box>
+                    </Grid>
+                  </>
+                ))}
+              </Grid>
+            </Form>
+          </div>
         )}
       </Formik>
     </>
