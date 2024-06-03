092c7c7d9378dc821db5a2548be3142c783212f7
arunmhere62 <120382469+arunmhere62@users.noreply.github.com>
2024-06-03 12:00:00 +0530

    changes : dashboard

--- src/pages/Dashboard/Dashboard-screen.tsx
+++ src/pages/Dashboard/Dashboard-screen.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Typography from '@mui/material/Typography'
 import InvoiceStatus from './InvoiceStatusChart'
 import { Grid } from '@mui/material'
+import InvoiceAmount from './InvoiceAmount'
 
 const Dashboard = () => {
     return (
-        <Grid container spacing={2}>
+        <Grid container spacing={2} px={0}>
             <Grid item xs={8}>
-                <h1>hello</h1>
+                <InvoiceAmount />
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={4} >
                 <InvoiceStatus />
             </Grid>
         </Grid>
