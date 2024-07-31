55207cc3069e8660e821ec78bddc6ac447056e52
DhineshT99 <dhinesht008@gmail.com>
2024-07-31 10:03:44 +0530

    added grid alignments

--- src/pages/Dashboard/Admin-dashboard/Dashboard-screen.tsx
+++ src/pages/Dashboard/Admin-dashboard/Dashboard-screen.tsx
@@ -1,5 +1,6 @@
-import AdminDashboardInvoiceOverviewAmount from "./InvoiceAmount";
-import AdminDashboardInvoicePieChart from "./InvoiceStatusChart";
+import { Grid } from "@mui/material";
+import AdminDashboardInvoiceOverviewAmount from "../Admin-dashboard/InvoiceAmount";
+import AdminDashboardInvoicePieChart from "../Admin-dashboard/InvoiceStatusChart";
 
 const AdminDashboardScreen = ({ adminData }: any) => {
   // Log the received data for debugging
@@ -17,12 +18,18 @@ const AdminDashboardScreen = ({ adminData }: any) => {
 
   return (
     <>
-      {invoiceOverviewAmountData && (
+       <Grid container spacing={2}>
+    {invoiceOverviewAmountData && (
+      <Grid item xs={8}>
         <AdminDashboardInvoiceOverviewAmount invoiceOverviewAmountData={invoiceOverviewAmountData} />
-      )}
-      {invoicePieChartData && (
+      </Grid>
+    )}
+    {invoicePieChartData && (
+      <Grid item xs={4}>
         <AdminDashboardInvoicePieChart invoicePieChartData={invoicePieChartData} />
-      )}
+      </Grid>
+    )}
+  </Grid>
     </>
   );
 };
