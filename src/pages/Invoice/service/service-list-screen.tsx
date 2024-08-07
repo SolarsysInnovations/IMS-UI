5351ff069a1b70550ccb9ca1b0035c20dff9a06b
DhineshT99 <dhinesht008@gmail.com>
2024-08-07 10:52:27 +0530

    inserted  new service files

---
+++ src/pages/Invoice/service/service-list-screen.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { Add } from '@mui/icons-material';
+import { useDispatch, useSelector } from 'react-redux';
+import { Box } from '@mui/material';
+import ServiceCreate from './service-create-screen';
+import ServiceEditScreen from './service-edit-screen'; // Import the edit screen
+import { AppDispatch } from '../../../redux-store/store';
+import { useGetServiceListQuery } from '../../../redux-store/api/injectedApis';
+import { clearServiceData } from '../../../redux-store/slices/serviceSlice';
+import usePathname from '../../../hooks/usePathname';
+import TableHeader from '../../../components/layouts/TableHeader';
+import GridDataUi from '../../../components/GridTable/GridData';
+import { columns } from '../../../constants/grid-table-data/service-table-data';
+import DialogBoxUi from '../../../components/ui/DialogBox';
+
+const ServicesList = () => {
+    const dispatch = useDispatch<AppDispatch>();
+
+    const { data: serviceList, error, isLoading, refetch } = useGetServiceListQuery();
+    const serviceStateDetails = useSelector((state: any) => state.globalState.data);
+    const [opendialogBox, setIsOpenDialogBox] = useState(false);
+
+    const buttons = [
+        {
+            label: 'Create Service List', icon: Add, onClick: () => {
+                dispatch(clearServiceData());
+                setIsOpenDialogBox(true);
+            }
+        },
+    ];
+
+    const pathname = usePathname();
+
+    return (
+        <>
+            <TableHeader headerName={pathname} buttons={buttons} />
+            <GridDataUi showToolbar={true} columns={columns || []} tableData={serviceList || []} checkboxSelection={false} />
+            {/* <ModalUi open={openModal} onClose={handleModalClose}>
+                <Box sx={{ marginTop: "15px" }}>
+                    {editMode ? (
+                        <ServiceEditScreen />
+                    ) : (
+                        <ServiceCreate />
+                    )}
+                </Box>
+            </ModalUi> */}
+            <DialogBoxUi
+                open={opendialogBox}
+                content={
+                    <>
+                        <ServiceCreate setIsOpenDialogBox={setIsOpenDialogBox} />
+                    </>
+                }
+                handleClose={() => {
+                    setIsOpenDialogBox(false)
+                }}
+            />
+        </>
+    );
+};
+
+export default ServicesList;
