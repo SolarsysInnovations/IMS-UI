938c761a28606480305a8317d722915fc9015233
arunmhere62 <120382469+arunmhere62@users.noreply.github.com>
2024-08-08 10:45:53 +0530

    merge main + afrin and changed authslice

--- src/pages/Dashboard/Admin-dashboard/Dashboard-screen.tsx
+++ src/pages/Dashboard/Admin-dashboard/Dashboard-screen.tsx
@@ -1,32 +1,32 @@
 import { Grid } from "@mui/material";
 import AdminDashboardInvoiceOverviewAmount from "../Admin-dashboard/InvoiceAmount";
 import AdminDashboardInvoicePieChart from "../Admin-dashboard/InvoiceStatusChart";
-
+import { useSelector } from "react-redux";
+import { selectUserDetails } from "../../../redux-store/auth/authSlice";
 const AdminDashboardScreen = ({ adminData }: any) => {
 
   // Check if adminData is undefined and provide default values if necessary
   if (!adminData) {
     return <div>No data available</div>;
   }
-
   // Provide default values based on the actual structure of adminData
   const invoiceOverviewAmountData = adminData.invoiceOverview || {};
   const invoicePieChartData = adminData.invoiceStatus || {};
 
   return (
     <>
-       <Grid container spacing={2}>
-    {invoiceOverviewAmountData && (
-      <Grid item xs={8}>
-        <AdminDashboardInvoiceOverviewAmount invoiceOverviewAmountData={invoiceOverviewAmountData} />
+      <Grid container spacing={2}>
+        {invoiceOverviewAmountData && (
+          <Grid item xs={8}>
+            <AdminDashboardInvoiceOverviewAmount invoiceOverviewAmountData={invoiceOverviewAmountData} />
+          </Grid>
+        )}
+        {invoicePieChartData && (
+          <Grid item xs={4}>
+            <AdminDashboardInvoicePieChart invoicePieChartData={invoicePieChartData} />
+          </Grid>
+        )}
       </Grid>
-    )}
-    {invoicePieChartData && (
-      <Grid item xs={4}>
-        <AdminDashboardInvoicePieChart invoicePieChartData={invoicePieChartData} />
-      </Grid>
-    )}
-  </Grid>
     </>
   );
 };
